8ebb13ce5509c2b125836004c62cf28349e153a9
Yiming Yang <yang.yihming@gmail.com>
2020-01-03 21:15:36 -0500

    Create packaging files

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,1 +1,5 @@
 from .harmony import harmonize
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ setup.py
@@ -0,0 +1,43 @@
+import versioneer
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requires = [
+    "torch",
+    "numpy",
+    "scikit-learn",
+]
+
+setup(
+    name="harmony-pytorch",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Pytorch implementation of Harmony algorithm on single-cell sequencing data integration",
+    long_description=long_description,
+    url="https://github.com/lilab-bcb/harmony-pytorch",
+    author="Yiming Yang, Bo Li",
+    author_email="yyang43@mgh.harvard.edu, bli28@mgh.harvard.edu",
+    classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="single-cell genomics data integration",
+    packages=find_packages(),
+    install_requires=requires,
+    python_requires="~=3.5",
+)
