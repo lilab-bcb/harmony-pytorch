c3fce726aa597bdddf56a432dac28adf9495e39c
Yiming Yang <yang.yihming@gmail.com>
2020-01-03 23:45:39 -0500

    fix bugs

--- harmony/harmony.py
+++ harmony/harmony.py
@@ -7,34 +7,33 @@ from torch.nn.functional import normalize
 from .utils import one_hot_tensor
 
 def harmonize(X, batch_mat, n_clusters = None, tau = 0, max_iter = 10, tol_harmony = 1e-4, tol_clustering = 1e-5, ridge_lambda = 1.0):
-    Z = torch.tensor(X)
-    n_cells = X.shape[0]
+    X_tensor = torch.tensor(X, dtype = torch.float)
+    Z = X_tensor.clone()
+    n_cells = X_tensor.shape[0]
 
-    batch_codes = batch_mat.cat.codes
+    batch_codes = batch_mat.astype('category').cat.codes.astype('category')
     n_batches = batch_codes.nunique()
-    N_b = torch.tensor(batch_codes.value_counts(sort = False))
+    N_b = torch.tensor(batch_codes.value_counts(sort = False).values)
     Pr_b = N_b.float() / n_cells
 
     Phi = one_hot_tensor(batch_codes)
 
-    R = torch.zeros(n_cells, n_batches)
-
-    n_cells = X.shape[0]
     if n_clusters is None:
         n_clusters = int(min(100, n_cells / 30))
+
+    R = torch.zeros(n_cells, n_clusters, dtype = torch.float)
 
     if tau <= 0:
         tau = np.random.randint(5, 21)
 
     theta = len(N_b) * (1 - torch.exp(- N_b.float() / (n_clusters * tau)) ** 2)
-
     
     # Initialization
     objectives_harmony = []
 
     for i in range(max_iter):
-        R, Y = clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol_clustering, objectives_harmony)
-        Z_new = correction(X, R, Phi, ridge_lambda)
+        R, Y = clustering(X_tensor, Z, Pr_b, Phi, R, n_clusters, theta, tol_clustering, objectives_harmony)
+        Z_new = correction(X_tensor, R, Phi, ridge_lambda)
         
         if is_convergent_harmony(objectives_harmony, tol = tol_harmony):
             break
@@ -42,14 +41,6 @@ def harmonize(X, batch_mat, n_clusters = None, tau = 0, max_iter = 10, tol_harmo
             Z = Z_new
 
     return Z
-
-def compute_objective(Y_norm, Z_norm, R, sigma, O, E, objective_arr):
-    kmeans_error = torch.sum(R * 2 * (1 - torch.matmul(Z_norm, Y_norm.t())))
-    entropy_term = sigma * torch.sum(R * torch.log(R))
-    diverse_penalty = sigma * torch.sum(theta.view(-1, 1).expand_as(E) * R * torch.matmul(Phi, torch.log(torch.div(O + 1, E + 1))))
-    objective = torch.sum(R * dist_mat) + entropy_term + diverse_penalty
-
-    objective_arr.append(objective)
 
 
 def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n_init = 10, random_state = 0, max_iter = 200, sigma = 0.1):
@@ -59,7 +50,7 @@ def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n
 
     kmeans = KMeans(n_clusters = n_clusters, init = 'k-means++', n_init = n_init, random_state = random_state, n_jobs = -1)
     kmeans.fit(Z)
-    Y = torch.tensor(kmeans.cluster_centers_, dtype = torch.float32)
+    Y = torch.tensor(kmeans.cluster_centers_, dtype = torch.float)
 
     Y_norm = normalize(Y, p = 2, dim = 1)
     Z_norm = normalize(Z, p = 2, dim = 1)
@@ -71,16 +62,16 @@ def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n
     R = torch.exp(R)
     R = torch.div(R, torch.sum(R, dim = 1).view(-1, 1))
 
-    E = torch.matmul(Pr_b.t(), torch.sum(R, dim = 0))
+    E = torch.matmul(Pr_b.view(-1, 1), torch.sum(R, dim = 0).view(1, -1))
     O = torch.matmul(Phi.t(), R)
 
     # Compute initialized objective.
     objectives_clustering = []
-    compute_objective(Y_norm, Z_norm, R, sigma, O, E, objectives_clustering)
+    compute_objective(Y_norm, Z_norm, R, Phi, theta, sigma, O, E, objectives_clustering)
 
     for i in range(max_iter):
         idx_list = np.arange(n_cells)
-        np.random.shuffle(id_list)
+        np.random.shuffle(idx_list)
         block_size = int(n_cells * 0.05)
         pos = 0
         while pos < len(idx_list):
@@ -90,7 +81,7 @@ def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n
     
             # Compute O and E on left out data.
             O -= torch.matmul(Phi_in.t(), R_in)
-            E -= torch.matmul(Pr_b.t(), torch.sum(R_in, dim = 0))
+            E -= torch.matmul(Pr_b.view(-1, 1), torch.sum(R_in, dim = 0).view(1, -1))
     
             # Update and Normalize R
             R_in = torch.exp(- 2 / sigma * (1 - torch.matmul(Z[idx_in,], Y_norm.t())))
@@ -101,7 +92,7 @@ def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n
     
             # Compute O and E with full data.
             O += torch.matmul(Phi_in.t(), R_in)
-            E += torch.matmul(Pr_b.t(), torch.sum(R_in, dim = 0))
+            E += torch.matmul(Pr_b.view(-1, 1), torch.sum(R_in, dim = 0).view(1, -1))
     
             pos += block_size
 
@@ -109,7 +100,7 @@ def clustering(X, Z, Pr_b, Phi, R, n_clusters, theta, tol, objectives_harmony, n
         Y_new = torch.matmul(R.t(), X)
         Y_new_norm = normalize(Y_new, p = 2, dim = 1)
 
-        compute_objective(Y_new_norm, Z_norm, R, sigma, O, E, objectives_clustering)
+        compute_objective(Y_new_norm, Z_norm, R, Phi, theta, sigma, O, E, objectives_clustering)
 
         if is_convergent_clustering(objectives_clustering, tol):
             objectives_harmony.append(objectives_clustering[-1])
@@ -126,15 +117,24 @@ def correction(X, R, Phi, ridge_lambda):
     n_batches = Phi.shape[1]
     Phi_1 = torch.cat((torch.ones(n_cells, 1), Phi), dim = 1)
 
-    Z = X
+    Z = X.clone()
     for k in range(n_clusters):
-        diag_R = torch.diag(R[:,k])
-        inv_mat = torch.inverse(torch.matmul(torch.matmul(Phi_1.t(), diag_R), Phi_1) + ridge_lambda * torch.eye(n_batches + 1, n_batches + 1))
-        W = torch.matmul(inv_mat, torch.matmul(torch.matmul(Phi_1.t(), diag_R), X))
+        Phi_t_diag_R = torch.matmul(Phi_1.t(), torch.diag(R[:,k]))
+        inv_mat = torch.inverse(torch.matmul(Phi_t_diag_R, Phi_1) + ridge_lambda * torch.eye(n_batches + 1, n_batches + 1))
+        W = torch.matmul(inv_mat, torch.matmul(Phi_t_diag_R, X))
         W[0, :] = 0
-        Z = X - torch.matmul(torch.matmul(Phi_1, W), diag_R)
+        Z -= torch.matmul(Phi_t_diag_R.t(), W)
 
     return Z
+
+
+def compute_objective(Y_norm, Z_norm, R, Phi, theta, sigma, O, E, objective_arr):
+    kmeans_error = torch.sum(R * 2 * (1 - torch.matmul(Z_norm, Y_norm.t())))
+    entropy_term = sigma * torch.sum(R * torch.log(R))
+    diverse_penalty = sigma * torch.sum(R * torch.matmul(Phi, theta.view(-1, 1).expand_as(E) * torch.log(torch.div(O + 1, E + 1))))
+    objective = kmeans_error + entropy_term + diverse_penalty
+
+    objective_arr.append(objective)
 
 
 def is_convergent_harmony(objectives_harmony, tol):
--- harmony/utils.py
+++ harmony/utils.py
@@ -4,7 +4,7 @@ def one_hot_tensor(X):
     ids = torch.LongTensor(X).view(-1, 1)
     n_row = X.shape[0]
     n_col = X.cat.categories.size
-    Phi = torch.zeros(n_row, n_col)
+    Phi = torch.zeros(n_row, n_col, dtype = torch.float)
     Phi.scatter_(dim = 1, index = ids, value = 1.)
 
     return Phi
